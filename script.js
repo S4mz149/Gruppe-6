f605ae7a412e6c7085f5d1b316af7f5599556de0
S4mz149 <samsonmatis27@gmail.com>
2026-02-06 00:50:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const track = document.querySelector('.carousel-track');
 let isDown = false, startX, scrollLeft;
 
+// souris
 track.addEventListener('mousedown', e => {
   isDown = true;
   startX = e.pageX - track.offsetLeft;
@@ -12,14 +13,14 @@ track.addEventListener('mousemove', e => {
   if(!isDown) return;
   e.preventDefault();
   const x = e.pageX - track.offsetLeft;
-  const walk = (x - startX) * 1; // vitesse
+  const walk = (x - startX);
   track.scrollLeft = scrollLeft - walk;
 });
 
-// touch pour mobile
+// mobile touch
 track.addEventListener('touchstart', e => { startX = e.touches[0].pageX; scrollLeft = track.scrollLeft; });
 track.addEventListener('touchmove', e => {
   const x = e.touches[0].pageX;
-  const walk = (x - startX) * 1;
+  const walk = (x - startX);
   track.scrollLeft = scrollLeft - walk;
 });
